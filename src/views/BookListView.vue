aebcadce38c865500483f26482d80e95168de638
robotcanfly <robotcanfly@gmail.com>
2024-08-27 13:10:24 +0700

    update page link

--- src/views/BookListView.vue
+++ src/views/BookListView.vue
@@ -1,9 +1,31 @@
 <script setup>
-
+    import { ref, onMounted } from 'vue'
     import { useBookStore } from '../stores/book';
     import { RouterLink } from 'vue-router';
-
+    import axios from 'axios'
     const bookStore = useBookStore();
+
+
+    const loaddata=async()=>{
+        try{
+            await axios.get(`http://116.206.127.158:3001/api/users/1`,{
+
+            })
+            .then((resp)=>{
+                console.log(resp.data);
+
+            }) 
+
+        }catch(error){
+            console.log('error', error);
+
+        }             
+    }
+
+    onMounted(async ()=>{
+        await loaddata();
+        
+    })    
 
 </script>
 
